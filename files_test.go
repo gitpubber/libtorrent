84685ce1b5a700d96771c5a529d11e6d53b4ff0c
Alexey Kuznetsov <axet@me.com>
2017-07-12 20:30:59 +0300

    add tests

---
+++ files_test.go
@@ -0,0 +1,21 @@
+package libtorrent
+
+import (
+	"log"
+	"regexp"
+	"testing"
+)
+
+func TestWildcast(t *testing.T) {
+	m := regexp.MustCompile(wildcardToRegex("*.mp3"))
+	var s string
+	s = "test/abc/123.mp3"
+	log.Println(s, m.MatchString(s))
+	s = "123.mp3"
+	log.Println(s, m.MatchString(s))
+	s = "123.mp4"
+	log.Println(s, m.MatchString(s))
+	m2 := regexp.MustCompile(wildcardToRegex("*64kb.mp3"))
+	s = "test/abc/123.mp3"
+	log.Println(s, m2.MatchString(s))
+}
