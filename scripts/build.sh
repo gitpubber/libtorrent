2daa9f9d9c344ff5bf73aa49012ca860101cf806
Alexey Kuznetsov <axet@me.com>
2018-08-28 21:08:54 +0300

    drop shared

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,8 +12,6 @@ GOPATH="$OUT"
 
 gomobile bind -o "$OUT/libtorrent.aar" "$@" gitlab.com/axet/libtorrent
 
-"$DIR/cpp_shared.sh"
-
 cat << EOF > "$OUT/build.gradle"
 configurations.maybeCreate("default")
 artifacts.add("default", file('libtorrent.aar'))
